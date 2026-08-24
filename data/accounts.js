a20e7a0267d8495e03f2336f0d1a7f2e18bfaa30
gragra-beep <ggragra37@gmail.com>
2026-08-24 19:33:20 +0300

    Create accounts.js

---
+++ data/accounts.js
@@ -0,0 +1,31 @@
+const PLAYER_ACCOUNTS = {
+  'ooo': {
+    cards: [
+      { cardId: 'card_001', level: 5, broken: true },
+      { cardId: 'card_002', level: 3, broken: true },
+      { cardId: 'card_003', level: 0, broken: false },
+      { cardId: 'card_005', level: 0, broken: false },
+      { cardId: 'card_006', level: 0, broken: false },
+    ],
+    summons: 0, lastEpic: 0, lastLegendary: 0, lastMythic: 0
+  }
+};
+
+function getPlayerInventory(login) {
+  return PLAYER_ACCOUNTS[login] || null;
+}
+
+function addCardToPlayer(login, cardId, level = 0) {
+  if (!PLAYER_ACCOUNTS[login]) {
+    PLAYER_ACCOUNTS[login] = {
+      cards: [], summons: 0, lastEpic: 0, lastLegendary: 0, lastMythic: 0
+    };
+  }
+  PLAYER_ACCOUNTS[login].cards.push({ cardId, level, broken: false });
+}
+
+function removeCardFromPlayer(login, cardIndex) {
+  if (PLAYER_ACCOUNTS[login]) {
+    PLAYER_ACCOUNTS[login].cards.splice(cardIndex, 1);
+  }
+}
