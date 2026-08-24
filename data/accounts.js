f2c73010fa6c0413dc1bc311a58c25861a834972
gragra-beep <ggragra37@gmail.com>
2026-08-24 21:02:33 +0300

    Update accounts.js

--- data/accounts.js
+++ data/accounts.js
@@ -9,6 +9,18 @@ const PLAYER_ACCOUNTS = {
     ],
     summons: 0, lastEpic: 0, lastLegendary: 0, lastMythic: 0
   }
+  
+  'каза': {  // ← ДОБАВЬ ЭТО
+    cards: [
+      { cardId: 'card_001', level: 5, broken: true },
+      { cardId: 'card_002', level: 3, broken: true },
+      { cardId: 'card_003', level: 0, broken: false },
+      { cardId: 'card_005', level: 0, broken: false },
+      { cardId: 'card_006', level: 0, broken: false },
+    ],
+    summons: 0, lastEpic: 0, lastLegendary: 0, lastMythic: 0
+  }
+};
 };
 
 function getPlayerInventory(login) {
