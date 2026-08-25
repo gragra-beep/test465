39575dbcd5fa00ddfd9b9f417094bfaee4647bc7
gragra-beep <ggragra37@gmail.com>
2026-08-25 18:52:13 +0300

    Update accounts.js

--- data/accounts.js
+++ data/accounts.js
@@ -7,7 +7,7 @@ const STARTER_CARDS = [
   { cardId: 'card_006', level: 0, broken: false }
 ];
 
-// Глобальный объект для хранения данных текущего игрока в памяти
+// Глобальный объект для хранения данных текущего игрока в памяти во время сессии
 window.PLAYER_ACCOUNTS = {};
 
 // Получить инвентарь текущего игрока
@@ -15,7 +15,7 @@ function getPlayerInventory(login) {
   return window.PLAYER_ACCOUNTS[login] || null;
 }
 
-// Добавить карту
+// Добавить карту в инвентарь
 function addCardToPlayer(login, cardId, level = 0) {
   if (!window.PLAYER_ACCOUNTS[login]) {
     window.PLAYER_ACCOUNTS[login] = { cards: [] };
@@ -23,7 +23,7 @@ function addCardToPlayer(login, cardId, level = 0) {
   window.PLAYER_ACCOUNTS[login].cards.push({ cardId, level, broken: false });
 }
 
-// Удалить карту по индексу
+// Удалить карту по индексу (для авто-распыления)
 function removeCardFromPlayer(login, cardIndex) {
   if (window.PLAYER_ACCOUNTS[login]) {
     window.PLAYER_ACCOUNTS[login].cards.splice(cardIndex, 1);
