d15f30887d23d3695a8519d6a3f84cbb0143f288
gragra-beep <ggragra37@gmail.com>
2026-08-25 11:16:05 +0300

    Update accounts.js

--- data/accounts.js
+++ data/accounts.js
@@ -4,13 +4,16 @@ const PLAYER_ACCOUNTS = {
   'ooo': {
     password: '1234',
     cards: [
-      { cardId: 'card_001', level: 5, broken: true },
-      { cardId: 'card_002', level: 3, broken: true },
+      { cardId: 'card_001', level: 0, broken: false },
+      { cardId: 'card_002', level: 0, broken: false },
       { cardId: 'card_003', level: 0, broken: false },
       { cardId: 'card_005', level: 0, broken: false },
       { cardId: 'card_006', level: 0, broken: false },
     ],
-    summons: 0, lastEpic: 0, lastLegendary: 0, lastMythic: 0
+    summons: 0, lastEpic: 0, lastLegendary: 0, lastMythic: 0,
+    completedLocs: [],
+    energy: 50,
+    silver: 100
   }
 };
 
@@ -23,21 +26,19 @@ const STARTER_CARDS = [
   { cardId: 'card_006', level: 0, broken: false }
 ];
 
-// Стартовые пройденные локации (чтобы maxEnergy = 80)
-const STARTER_COMPLETED_LOCS = [0];
-
 // Создание нового аккаунта
 function createAccount(login, password) {
   PLAYER_ACCOUNTS[login] = {
     password: password,
-    cards: JSON.parse(JSON.stringify(STARTER_CARDS)), // глубокая копия
+    cards: JSON.parse(JSON.stringify(STARTER_CARDS)),
     summons: 0,
     lastEpic: 0,
     lastLegendary: 0,
     lastMythic: 0,
-    completedLocs: [...STARTER_COMPLETED_LOCS],
+    completedLocs: [],
     energy: 50,
-    silver: 100
+    silver: 100,
+    lastEnergyUpdate: Date.now()
   };
 }
 
